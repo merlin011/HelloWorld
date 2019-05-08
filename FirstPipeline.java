4c73f8ebad20c74d94523c2297158c1bfb7eb19f
merlin011 <50401809+merlin011@users.noreply.github.com>
2019-05-08 15:28:25 +0530

    Add files via upload

--- FirstPipeline.java
+++ FirstPipeline.java
@@ -2,7 +2,7 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class HelloWorld {
+public class FirstPipeline {
   public static void main(String[] args) {
     System.out.println("This is my first pipeline");
   }
